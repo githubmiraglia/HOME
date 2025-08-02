2d881587655bb683c8c06c30f66277e6043c1d3b
Luis Miraglia <luis.miraglia@icloud.com>
2025-08-02 19:35:53 -0300

    v45

--- family-gallery/src/components/Playvideo.tsx
+++ family-gallery/src/components/Playvideo.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import "./css/PlayVideo.css";
+import "./css/Playvideo.css";
 import { GLOBAL_BACKEND_URL } from "../App";
 
 interface PlayVideoProps {
